6d3d8f3666b7d0a576fba84d7ef0671921d6c9c6
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-01 19:31:25 -0800

    Completed using LinkContainer to setup the hrefs in the Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            {" "}
-            <img src={logo} alt="ProShop" />
-            ProShop
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="ProShop" />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav className="ms-auto">
-            <Nav.Link href="/cart">
-              <FaShoppingCart /> Cart
-            </Nav.Link>
-            <Nav.Link href="/login">
-              <FaUser /> Sign In
-            </Nav.Link>
-            <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-          </Nav>
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "bootstrap/dist/css/bootstrap.min.css";
+import { ReactDOM } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App.jsx";
 
+// Creates a route using createRoutesFromElements
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // App.js becomes your Layout
+    <Route path="/" element={<App />}>
+      {/* index = true - Only shows the HomeScreen - May show a big if there's an issue. */}
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
