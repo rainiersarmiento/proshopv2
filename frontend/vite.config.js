85fac5f69941b3eda3ea468477bf54cee7705883
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-02 22:02:08 -0800

    connecting the backend with the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    eslintPlugin({
+      // setup the plugin
+      cache: false,
+      include: ["./src/**/*.js", "./src/**/*.jsx"],
+      exclude: [],
+    }),
+  ],
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000",
+      "/uploads": "http://localhost:8000",
+    },
+  },
+});
