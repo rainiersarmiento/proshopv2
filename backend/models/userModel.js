166e0b3b3711a15b2126ce53ae6b8d2ca6c822ae
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-04 15:56:44 -0800

    orderModel created

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,1 +1,28 @@
 // Note the singular form in 'userModel.js
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
