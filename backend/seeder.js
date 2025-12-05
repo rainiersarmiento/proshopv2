17c3167ea634bb0f999c59444016a58607751ea2
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-04 23:14:18 -0800

    seeder for data deletion and creation

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import uri from process.env.MONGO_URI
+import uri from process.env.MONGO_URI;
 import colors from "colors";
 import users from './data/users.js';
 import products from './data/products.js';
@@ -17,7 +17,44 @@ const importData = async() => {
         await Product.deleteMany();
         await User.deleteMany();
         const createdUsers = await User.insertMany(users); 
+        const adminUser = createdUsers[0]._id;
+
+        const sampleProducts = products.map((product => {
+            return {...product, user: adminUser};
+        }));
+            /**
+            {
+                name: "iPhone 11 Pro 256GB Memory",
+                image: "/images/phone.jpg",
+                description:
+                "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+                brand: "Apple",
+                category: "Electronics",
+                price: 599.99,
+                countInStock: 7,
+                rating: 4.0,
+                numReviews: 8,
+                user: adminUser
+            },
+             */
+        // inserts products into the database
+        await Product.insertMany(sampleProducts);
+        console.log('Data imported'.green.inverse);
+        process.exit();
     } catch (error) {
-        
+        console.error(`${error}`.red.inverse)
+        process.exit(1);
+    }
+}
+const destroyData = async () => {
+    try{
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+        console.log('Data destroyed!'.red.inverse);
+        process.exit();
+    }catch(error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
     }
 }
