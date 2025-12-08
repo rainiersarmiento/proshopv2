05b39239842a37d0c4ea6ca4f2f9c4c47319d0d0
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-07 21:03:11 -0800

    added store.js and connected it using provider in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,8 @@ import ProductScreen from "./screens/ProductScreen.jsx";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store";
 
 // Creates a route using createRoutesFromElements
 const router = createBrowserRouter(
@@ -29,6 +31,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
