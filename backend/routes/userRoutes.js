d36d7e5ee86c59dcc7b285a2886d38243d4e6250
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-11 16:28:02 -0800

    base request for all User CRUD functionality - receive text only

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -22,9 +22,18 @@ import {
 
 // Will need middleware to allow ONLY admin access
 router.route("/").post(registerUser).get(getUsers);
+
+/**
+ * route to api/users/id and can be chained like an if statement
+ * if delete request, call delete
+ * if get, call getUserById
+ * if put request then get updateUser
+ */
+router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
+
+// Why .post and not route? Its only a post request to logout, not a get or post so no need for router
 router.post("/logout", logoutUser);
 router.post("/login", loginUser);
 router.route("/profile").get(getUserProfile).put(updateUserProfile);
-router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
 
 export default router;
