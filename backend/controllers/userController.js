ca699a18235d6e525f61f41bd2ad151146cea8ee
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-14 17:45:44 -0800

    began register function call

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,7 +47,29 @@ const loginUser = asyncHandler(async (req, res, next) => {
 // @route POST /api/users/login
 // @access Public
 const registerUser = asyncHandler(async (req, res, next) => {
-  res.send("register user");
+  // res.send("register user");
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc Logout user & clear HTTP cookie on server
