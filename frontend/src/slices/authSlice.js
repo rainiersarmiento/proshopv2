27d5486c8609158b4f30d66f16e409b8be5a67c7
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-15 13:34:13 -0800

    created authSlice.js and added to store.js

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+// Used to set user's credentials to local storage.
+// Will handle clearing local storage when logging out.
+const initialState = {
+  useInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+export default authSlice.reducer;
