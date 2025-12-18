6dd81d560fd37f4745d8abcfce792893318dd1fc
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-18 13:05:38 -0800

    addOrderItems POST request completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,24 +5,55 @@ import Order from "../models/orderModel.js";
 // @route POST /api/orders
 // @access Private
 const addOrderItems = asyncHandler(async (req, res, next) => {
-  console.log("Router called add order items.");
-  res.send("Add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    // Check if order items array exists but its empty
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined, // Set to undefined because product will take place of the ID
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc Get logged in user order
 // @route GET /api/orders/myorders
 // @access Private
 const getMyOrders = asyncHandler(async (req, res, next) => {
-  console.log("Router called add order items.");
-  res.send("get my orders");
+  //   console.log("Router called add order items.");
+  //   res.send("get my orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc Get order by ID
 // @route GET /api/orders/:id
 // @access Private
 const getOrderById = asyncHandler(async (req, res, next) => {
-  console.log("Router called getOrderById");
-  res.send("get order by ID");
+  const order = await Order.findById(req.order._id);
 });
 
 // @desc Update order to paid
