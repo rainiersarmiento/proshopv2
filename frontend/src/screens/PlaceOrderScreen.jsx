89557f9f162d8dc3798e2d47d567faca3bcddd4c
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-18 15:54:39 -0800

    completed imports for PlaceOrderScreen.jsx

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,6 +1,10 @@
-import React from "react";
-
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
+import CheckoutSteps from "../components/CheckoutSteps";
 const PlaceOrderScreen = () => {
+  const navigate = useNavigate();
   return <div>PlaceOrderScreen</div>;
 };
 
