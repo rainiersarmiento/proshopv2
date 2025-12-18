f865ec91c32d5a026d4b6cb0a0b49f085e609ecf
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-18 15:56:44 -0800

    added useEffect to placeorderscreen

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -5,6 +5,15 @@ import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import CheckoutSteps from "../components/CheckoutSteps";
 const PlaceOrderScreen = () => {
   const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate("/shipping");
+    } else if (!cart.paymentMethod) {
+      navigate("/payment");
+    }
+  }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
   return <div>PlaceOrderScreen</div>;
 };
 
