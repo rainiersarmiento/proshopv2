a4ad5647c2342da8e7ec1d1ee1df90502f4ba1b4
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-19 19:16:49 -0800

    useEffect needed?

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,21 +1,38 @@
 import { useState, useEffect } from "react";
-import { Table, Form, Button, Row, Col } from "react-router-bootstrap";
-import LinkContainer from "react-router-bootstrap";
+import { Table, Button, Form, Row, Col } from "react-bootstrap";
+// import LinkContainer from "react-router-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import Message from "../components/Message";
-import Loader from "../components/Loader";
-import { toast } from "react-toastify";
-import { useProfileMutation } from "../slices/usersApiSlice";
-import { setCredentials } from "../slices/authSlice";
-
+// import Message from "../components/Message";
+// import Loader from "../components/Loader";
+// import { toast } from "react-toastify";
+// import { useProfileMutation } from "../slices/usersApiSlice";
+// import { setCredentials } from "../slices/authSlice";
 const ProfileScreen = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const { userInfo } = useSelector((state) => state.auth);
+  const [name, setName] = useState(userInfo?.name || "");
+  const [email, setEmail] = useState(userInfo?.email || "");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const dispatch = useDispatch();
 
-  const { userInfo } = useSelector((state) => state.auth);
-  return <div>ProfileScreen</div>;
+  //   useEffect(() => {
+  //     if (userInfo) {
+  //       setName(userInfo.name);
+  //       setEmail(userInfo.email);
+  //     }
+  //   }, [userInfo]);
+
+  //   const submitHandler = (e) => {
+  //     e.preventDefault();
+  //     console.log("Submit Clicked");
+  //   };
+  return (
+    <Row>
+      {/* Contains user data */}
+      <Col md={3}>Column</Col>
+      {/* Contains the orders */}
+      <Col md={9}>Column</Col>
+    </Row>
+  );
 };
 export default ProfileScreen;
