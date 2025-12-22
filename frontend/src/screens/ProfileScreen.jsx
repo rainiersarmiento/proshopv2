b753c990a9e46f170a6cd95f0502c0046b05bbcb
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-22 14:59:42 -0800

    Updated ProfileScreen to update name, email, or password

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { Table, Button, Form, Row, Col } from "react-bootstrap";
-// import LinkContainer from "react-router-bootstrap";
+import LinkContainer from "react-router-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-// import Message from "../components/Message";
-// import Loader from "../components/Loader";
-// import { toast } from "react-toastify";
-// import { useProfileMutation } from "../slices/usersApiSlice";
-// import { setCredentials } from "../slices/authSlice";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { toast } from "react-toastify";
+import { useProfileMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
 const ProfileScreen = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const [name, setName] = useState(userInfo?.name || "");
@@ -15,16 +15,35 @@ const ProfileScreen = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const dispatch = useDispatch();
 
-  //   useEffect(() => {
-  //     if (userInfo) {
-  //       setName(userInfo.name);
-  //       setEmail(userInfo.email);
-  //     }
-  //   }, [userInfo]);
+  const [updateProfile, { isLoading: loadingUpdateProfile, error }] =
+    useProfileMutation();
 
-  const submitHandler = (e) => {
+  // useEffect(() => {
+  //   if (userInfo) {
+  //     setName(userInfo.name);
+  //     setEmail(userInfo.email);
+  //   }
+  // }, [userInfo, userInfo.email, userInfo.name]);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("Submit Clicked");
+    // console.log("Submit Clicked");
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          name,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCredentials(res));
+        toast.success("Profile update successfully");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
   };
   return (
     <Row>
@@ -41,6 +60,41 @@ const ProfileScreen = () => {
                 onChange={(e) => setName(e.target.value)}
               ></Form.Control>
             </Form.Group>
+
+            <Form.Group controlId="email" className="my-2">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="password" className="my-2">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter new password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="confirmPassword" className="my-2">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Re-enter password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant="primary" className="my-2">
+              Update
+            </Button>
+            {loadingUpdateProfile && <Loader />}
           </Form>
         </h2>
       </Col>
