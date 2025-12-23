d71d6414857eae9a97d856a4bd9462ab37d685f0
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-23 15:36:15 -0800

    working adminroute

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const AdminRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth.userInfo);
+  const { userInfo } = useSelector((state) => state.auth);
   return userInfo && userInfo.isAdmin ? (
     <Outlet />
   ) : (
