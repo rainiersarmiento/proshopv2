27168564898d6e81c17900f9b3ba34aeeb19aeab
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-27 17:45:07 -0800

    Delete functionality completed.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,7 @@ import {
   getProducts,
   getProductById,
   updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 import { createProduct } from "../controllers/productController.js";
@@ -25,27 +26,31 @@ router.route("/").get(getProducts).post(protect, admin, createProduct);
 // DESC : GET A SINGLE PRODUCT
 // ROUTE : '/api/products/:id'
 // @ACCESS  : Public
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 // DESC :  CREATE A PRODUCTS
 // ROUTE : '/api/products/
 // ACCESS : Public
-router.route("/").post((req, res) => {
-  console.log("POST request called to /api/products");
-});
+// router.route("/").post((req, res) => {
+//   console.log("POST request called to /api/products");
+// });
 
 // DESC : ALTER A PRODUCT
 // ROUTE : '/api/products/:id'
 // ACCESS : Public
-router.put("/:id", (req, res) =>
-  console.log(`PUT request called to api/products/${req.params.id}`)
-);
+// router.put("/:id", (req, res) =>
+//   console.log(`PUT request called to api/products/${req.params.id}`)
+// );
 
-// DESC : DELETE A PRODUCT
-// ROUTE : '/api/products/:id'
-// ACCESS : Public
-router.delete("/:id", (req, res) =>
-  console.log(`DELETE request sent to \'/api/products/${req.params.id}\'`)
-);
+// // DESC : DELETE A PRODUCT
+// // ROUTE : '/api/products/:id'
+// // ACCESS : Public
+// router.delete("/:id", (req, res) =>
+//   console.log(`DELETE request sent to \'/api/products/${req.params.id}\'`)
+// );
 
 export default router;
