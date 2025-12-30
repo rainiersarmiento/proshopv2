4f9983dcdd1abe2fd4551f4fddd5543a7373ffbc
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2025-12-29 23:05:41 -0800

    Options for option tag set

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -194,6 +194,11 @@ const ProductScreen = () => {
                           onChange={(e) => setRating(Number(e.target.value))}
                         >
                           <option value="">Select...</option>
+                          <option value="1">1 - Poor</option>
+                          <option value="2">2 - Fair</option>
+                          <option value="3">3 - Good</option>
+                          <option value="4">4 - Very Good</option>
+                          <option value="5">5 - Excellent</option>
                         </Form.Control>
                       </Form.Group>
                     </Form>
