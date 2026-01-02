8f0e7584cdb2111c060f6520b01605b1d3d55793
Rainier Sarmiento <rensarmie@csu.fullerton.edu>
2026-01-01 21:33:18 -0800

    Cleared out unused variable warnings importing un-needed React from react

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Alert } from "react-bootstrap";
 const Message = ({ variant, children }) => {
   return <Alert variant={variant}>{children}</Alert>;
--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,6 +1,5 @@
 import { Pagination } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { Link } from "react-router-dom";
 
 const Paginate = ({ pages, page, isAdmin = false, keyword = "" }) => {
   return (
